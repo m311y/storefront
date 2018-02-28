7ac05f2afa5c0add5e72bc721ea8d138147ccf33
Melissa Hardwick <melissaohardwick@gmail.com>
2018-02-27 19:22:43 -0500

    more set up of MVC

---
+++ storefront/Models/ItemListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace storefront.Models
+{
+    public class ItemListViewModel
+    {
+        public List<ItemViewModel> Items { get; set; }
+        public int TotalItems { get; set; }
+    }
+}
---
+++ storefront/Models/ItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace storefront.Models
+{
+    public class ItemViewModel
+    {
+
+        public int? ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string ItemDetail => ItemName + " " + ItemPrice;
+
+    }
+}
