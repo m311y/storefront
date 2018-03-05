5bff994a953a7282cf33da2b15f28b9458f92cff
Melissa Hardwick <melissaohardwick@gmail.com>
2018-03-05 09:51:30 -0500

    addition of userview and models

---
+++ storefront/Models/UserListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace storefront.Models
+{
+    public class UserListViewModel
+    {
+        public List<UserViewModel> Items { get; set; }
+    }
+}
---
+++ storefront/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace storefront.Models
+{
+    public class UserViewModel
+    {
+        public int? UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
